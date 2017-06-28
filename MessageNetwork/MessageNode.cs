c269101124995fe0e96b2d2a971ffb6473fa0cee
WiiPlayer2 <waldetomme@gmail.com>
2017-06-29 00:49:49 +0200

    Added server setup;

--- MessageNetwork/MessageNode.cs
+++ MessageNetwork/MessageNode.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MessageNetwork
@@ -17,11 +18,12 @@ namespace MessageNetwork
         private Node<T> rootNode;
 
         private TcpListener tcpListener;
+        private Thread acceptThread;
         private TcpClient tcpClient;
 
         public MessageNode(AsymmetricCipherKeyPair keyPair)
         {
-            if(keyPair == null)
+            if (keyPair == null)
             {
                 throw new ArgumentNullException(nameof(keyPair));
             }
@@ -41,14 +43,14 @@ namespace MessageNetwork
             if (receiver != null)
             {
                 var node = rootNode.Find(receiver);
-                if(node != null)
+                if (node != null)
                 {
                     node.Session.SendMessage(receiver, message, payload);
                 }
             }
             else
             {
-                foreach(var node in rootNode.Children)
+                foreach (var node in rootNode.Children)
                 {
                     node.Session.SendMessage(null, message, payload);
                 }
@@ -70,17 +72,45 @@ namespace MessageNetwork
             throw new NotImplementedException();
         }
 
-        public void StartServer()
+        public void Setup()
         {
-            throw new NotImplementedException();
+            if (tcpListener != null && acceptThread != null)
+            {
+                tcpListener.Start();
+                acceptThread = new Thread(AcceptLoop);
+                acceptThread.Start();
+            }
         }
 
-        public void ConnectToServer(string host, int port)
+        private void AcceptLoop()
         {
+            while(true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                var cryptStream = new CryptedStream(client.GetStream(), keyPair);
+                if(cryptStream.Setup(key =>
+                {
+                    throw new NotImplementedException();
+                }))
+                {
+                    var session = new NodeSession<T>(cryptStream);
+                    session.InternalExceptionOccured += Session_InternalExceptionOccured;
+                    session.RawMessageReceived += Session_RawMessageReceived;
+
+                    var node = new Node<T>(session);
+                    rootNode.AddChild(node);
+                }
+            }
+        }
+
+        public void Setup(string host, int port)
+        {
+            Setup();
+
             tcpClient = new TcpClient();
             tcpClient.Connect(host, port);
             var cryptStream = new CryptedStream(tcpClient.GetStream(), keyPair);
-            if(cryptStream.Setup())
+            if (cryptStream.Setup())
             {
                 var session = new NodeSession<T>(cryptStream);
                 session.InternalExceptionOccured += Session_InternalExceptionOccured;
@@ -93,15 +123,11 @@ namespace MessageNetwork
 
         private void Session_RawMessageReceived(NodeSession<T> sender, NodeMessage<T> message, byte[] payload)
         {
-            if(message.Receiver != null)
+            if (message.Receiver != null)
             {
-                if(message.Receiver.Equals(keyPair.Public))
+                if (message.Receiver.Equals(keyPair.Public))
                 {
-                    if (message.IsSystemMessage)
-                    {
-                        HandleSystemMessage(message.SystemMessage);
-                    }
-                    //TODO: Handle message event
+                    HandleMessage(message);
                 }
                 else
                 {
@@ -110,22 +136,18 @@ namespace MessageNetwork
             }
             else
             {
-                if(message.IsSystemMessage)
-                {
-                    HandleSystemMessage(message.SystemMessage);
-                }
-
-                foreach(var node in rootNode.Children.Where(o => !o.PublicKey.Equals(sender.ReceivedPublicKey)))
+                foreach (var node in rootNode.Children.Where(o => !o.PublicKey.Equals(sender.ReceivedPublicKey)))
                 {
                     node.Session.SendMessage(message, payload);
                 }
+                HandleMessage(message);
             }
             throw new NotImplementedException();
         }
 
-        private void HandleSystemMessage(SystemMessage msg)
+        private void HandleMessage(NodeMessage<T> msg)
         {
-
+            throw new NotImplementedException();
         }
 
         private void Session_InternalExceptionOccured(object sender, MessageNetworkException e)
