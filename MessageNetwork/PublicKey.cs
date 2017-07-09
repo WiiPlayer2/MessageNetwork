5f47e821b0a24c54523a744460f196c95526b59c
WiiPlayer2 <waldetomme@gmail.com>
2017-07-09 16:19:42 +0200

    PublicKey is now parseable and compareable

--- MessageNetwork/PublicKey.cs
+++ MessageNetwork/PublicKey.cs
@@ -14,9 +14,64 @@ namespace MessageNetwork
 
         public byte[] ExponentBytes { get; set; }
 
+        public override string ToString()
+        {
+            return $"{string.Concat(ModulusBytes.Select(o => o.ToString("X2")))}:{string.Concat(ExponentBytes.Select(o => o.ToString("X2")))}"
+        }
+
+        public static PublicKey Parse(string str)
+        {
+            var split = str.Split(':');
+            var modBytes = new byte[split[0].Length / 2];
+            var expBytes = new byte[split[1].Length / 2];
+
+            for(var i = 0; i < modBytes.Length; i++)
+            {
+                modBytes[i] = byte.Parse(split[0].Substring(i * 2, 2), System.Globalization.NumberStyles.HexNumber);
+            }
+            for (var i = 0; i < expBytes.Length; i++)
+            {
+                expBytes[i] = byte.Parse(split[1].Substring(i * 2, 2), System.Globalization.NumberStyles.HexNumber);
+            }
+
+            return new PublicKey()
+            {
+                ModulusBytes = modBytes,
+                ExponentBytes = expBytes,
+            };
+        }
+
+        public static bool operator ==(PublicKey key1, PublicKey key2)
+        {
+            return key1?.ToString() == key2?.ToString();
+        }
+
+        public static bool operator !=(PublicKey key1, PublicKey key2)
+        {
+            return !(key1 == key2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is PublicKey)
+            {
+                return this == (PublicKey)obj;
+            }
+            if(obj is RsaKeyParameters)
+            {
+                return this == (PublicKey)(RsaKeyParameters)obj;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
         public static implicit operator RsaKeyParameters(PublicKey key)
         {
-            if(key == null)
+            if (key == null)
             {
                 return null;
             }
